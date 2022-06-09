325cea53a102859549ac6caca52909dfe975b796
gustavo@mumuki.org <gustavo@mumuki.org>
2022-06-09 16:09:03 +0000

    Mumuki Export on 2022-06-09 16:09:03 +0000

--- 00003_Muchas despedidas/extra.js
+++ 00003_Muchas despedidas/extra.js
@@ -1,3 +1,10 @@
-function despedir(nombre){
+function despedir(nombre) /*<elipsis-for-student@*/ {
   return "Hasta luego "+ nombre;
-}
+} /*@elipsis-for-student>*/
+
+// Retorna una despedida al nombre recibido como argumento
+//
+// Por ejemplo:
+//
+//  ム despedir("Guille")
+//  "Hasta luego Guille"
